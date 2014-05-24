ac1ef6f61606b075aa719ff4832374d4cf0f7bc6
Takashi Yoshida <takashi@takashiyoshida.org>
2014-05-24 18:31:03 +0800

    Fix indentation and quotation errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 # This is a placeholder class.
 class picturelife {
   package { 'Picturelife':
-    ensure => installed,
+    ensure   => installed,
     provider => 'appdmg',
-    source => "https://s3.amazonaws.com/download.picturelife.com/mac/Picturelife.dmg"
+    source   => 'https://s3.amazonaws.com/download.picturelife.com/mac/Picturelife.dmg'
   }
 }
